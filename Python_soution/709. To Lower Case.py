4f450b80a79c5e50b7318fe945d15feaa1e5fb94
Jimmy <mrxujimmy@gmail.com>
2019-02-13 12:02:04 +1100

    inital solution to 709

---
+++ Python_soution/709. To Lower Case.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def toLowerCase(self, str):
+        """
+        :type str: str
+        :rtype: str
+        """
+
+        '''
+        dont have an idea, may just use toLowerCase()
+        '''
+        return str.lower()
+
+        '''
+        google for:
+        1. string.tolowercase() --- string.lower()
+        '''
