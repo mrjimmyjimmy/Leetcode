d46a661c1a54f720c6e8718d88217e12e1ba6604
Jimmy <mrxujimmy@gmail.com>
2019-04-22 15:05:01 +1000

    innitial solution for 1.twoSum

---
+++ Java_solution/TwoSum_1.java
@@ -0,0 +1,20 @@
+
+public class TwoSum_1 {
+    public int[] twoSum(int[] nums, int target) {
+        int indexOne = 0;
+        int indexTwo = 0;
+        for(int i = 0; i < nums.length; i ++){
+            for(int j = i + 1; j < nums.length; j ++){
+                if (target == nums[i] + nums[j]){
+                    // System.out.println("find");
+                    indexOne = i;
+                    indexTwo = j;
+                }
+            }
+        }
+        int[] index = {indexOne, indexTwo};
+        return index;
+    }
+    
+// iterate array, find j = traget - i
+}
