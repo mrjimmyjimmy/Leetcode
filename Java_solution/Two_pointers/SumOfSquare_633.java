0247d7fdafb23f3c4b64ab6cc12cafa1961f83c5
Jimmy <mrxujimmy@gmail.com>
2019-07-16 17:09:20 +1000

    633

--- Java_solution/Two_pointers/SumOfSquare_633.java
+++ Java_solution/Two_pointers/SumOfSquare_633.java
@@ -1,5 +1,19 @@
 package Two_pointers;
 
 public class SumOfSquare_633 {
-
+    public boolean judgeSquareSum(int c) {
+        int min = 0;
+        int max = (int) Math.sqrt(c);
+        while(min <= max){
+            int square = min*min + max*max;
+            if(square == c){
+                return true;
+            } else if(square < c){
+                min++;
+            } else{
+                max--;
+            }
+        }
+        return false;
+    }
 }
